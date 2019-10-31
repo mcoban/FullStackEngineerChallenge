29af9489ec4153770347c9d67d017f38970359ae
root <root@vmi219031.contaboserver.net>
2019-10-31 11:34:33 +0100

    after deleted gitignore files

---
+++ backend/resources/employee/employee.model.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
+const employeeSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  fullName: {
+    type: String,
+    required: true
+  }
+})
+
+employeeSchema.pre('save', function (next) {
+  if (!this.isModified('password')) {
+    return next()
+  }
+
+  bcrypt.hash(this.password, 8, (err, hash) => {
+    if (err) {
+      return next(err)
+    }
+    this.password = hash
+    next()
+  })
+})
+
+employeeSchema.methods.checkPassword = function (password) {
+  const passwordHash = this.password
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(password, passwordHash, (err, same) => {
+      if (err) {
+        return reject(err)
+      }
+      resolve(same)
+    })
+  })
+}
+
+module.exports.Employee = mongoose.model('employee', employeeSchema)
